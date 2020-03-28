8264b785a5750ef26a78a71e7c514f80ec7e0a2c
lihaohua <lihaohua@gtland.cn>
2020-03-29 00:51:23 +0800

    table组件封装

---
+++ blog_admin/src/components/tables.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="table">
+    <el-table
+      ref="refBlogTable"
+      :data="data"
+      tooltip-effect="dark"
+      style="width: 100%"
+      border
+      @selection-change="handleSelectionChange"
+    >
+      <template v-for="(item, index) in column">
+        <!-- selection -->
+        <el-table-column
+          v-if="item.type === 'selection'"
+          :key="index"
+          type="selection"
+          :width="item.width || 200"
+        />
+        <!-- 其他属性 -->
+        <template v-else>
+          <!-- 渲染操作列 -->
+          <template v-if="item.renderToolBox">
+            <el-table-column
+              :key="index"
+              :prop="item.prop"
+              :label="item.label"
+              :width="item.width"
+              :align="item.align"
+              :show-overflow-tooltip="item.tooltip"
+            >
+              <template
+                slot-scope="scope"
+              >
+                <el-button
+                  v-for="(render, i) in item.renderToolBox(scope.row)"
+                  v-show="render"
+                  :key="i"
+                  :type="render.type"
+                  :size="render.size"
+                  @click="clickHandler(render, scope.row)"
+                >
+                  {{ render.text }}
+                </el-button>
+              </template>
+            </el-table-column>
+          </template>
+          <!-- 渲染需要format的属性 -->
+          <el-table-column
+            v-else-if="item.format"
+            :key="index"
+            :prop="item.prop"
+            :label="item.label"
+            :width="item.width"
+            :align="item.align"
+            :show-overflow-tooltip="item.tooltip"
+          >
+            <template slot-scope="scope">
+              <!-- <span>{{scope.row[item.prop]}}</span> -->
+              <span>{{ item.format(scope.row) }}</span>
+            </template>
+          </el-table-column>
+          <!-- 渲染不做任何处理的属性 -->
+          <el-table-column
+            v-else
+            :key="index"
+            :prop="item.prop"
+            :label="item.label"
+            :width="item.width"
+            :align="item.align"
+            :show-overflow-tooltip="item.tooltip"
+          />
+        </template>
+      </template>
+    </el-table>
+  </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable */
+import { Component, Vue, Prop, Emit } from 'vue-property-decorator'
+
+@Component({
+  components: {}
+})
+export default class Home extends Vue {
+  @Prop({default: []}) column
+  @Prop({default: []}) data
+  @Emit('selectChange') selectChange(selection){}
+  
+  blogModel: object = {
+    keyWord: '',
+    articleType: 'html',
+    pageNe: 1,
+    total: 50
+  }
+  handleSelectionChange(selection){
+    this.selectChange(selection)
+  }
+  clickHandler(render, row){
+    render.callback(row)
+  }
+  
+}
+</script>
+
+<style lang="scss" scoped>
+  .table{
+    
+  }
+</style>
--- blog_admin/src/views/blogList/index.vue
+++ blog_admin/src/views/blogList/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="blog-list">
-    <el-button style="margin-bottom: 15px;" size="mini" type="primary" @click="$router.push({path: '/createBlog'})">创建博客</el-button>
+    <el-button
+      style="margin-bottom: 15px;"
+      size="mini"
+      type="primary"
+      @click="$router.push({path: '/createBlog'})"
+    >
+      创建博客
+    </el-button>
     <el-form
       :inline="true"
       :model="blogModel"
@@ -57,58 +64,12 @@
       </el-form-item>
     </el-form>
     <hr>
-    <el-table
-      ref="refBlogTable"
+    <Table
       :data="blogTableData"
-      tooltip-effect="dark"
-      style="width: 100%"
-      border
-      @selection-change="handleSelectionChange"
-    >
-      <el-table-column
-        type="selection"
-        width="55"
-      />
-      <el-table-column
-        prop="title"
-        show-overflow-tooltip
-        label="文章标题"
-      />
-      <el-table-column
-        label="创建时间"
-        width="200"
-      >
-        <template slot-scope="scope">
-          {{ scope.row.createDate }}
-        </template>
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="作者"
-        width="120"
-      />
-      <el-table-column
-        prop="address"
-        label="操作"
-        width="220"
-      >
-        <template slot-scope="scope">
-          <el-button
-            v-show="scope.row.status === 1"
-            size="mini"
-            @click="$router.push({path: '/editBlog', query:{id: 1}})"
-          >
-            编辑
-          </el-button>
-          <el-button size="mini">
-            删除
-          </el-button>
-          <el-button size="mini">
-            下架
-          </el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+      :column="column"
+      @selectChange="selectChange"
+    />
+      
     <div class="pagination-wrap">
       <el-pagination
         :current-page="blogModel.pageNo"
@@ -127,9 +88,12 @@
 /* eslint-disable */
 // @ is an alias to /src
 import { Component, Vue } from 'vue-property-decorator'
+import Table from '@/components/tables.vue'
 
 @Component({
-  components: {}
+  components: {
+    Table
+  }
 })
 export default class Home extends Vue {
   blogModel: object = {
@@ -138,16 +102,118 @@ export default class Home extends Vue {
     pageNe: 1,
     total: 50
   }
+  column: Array<any> = [
+    {
+      type: 'selection',
+      width: '50'
+    },
+    {
+      prop: "title",
+      label: "文章标题",
+      width: "",
+      tooltip: 'show-overflow-tooltip'
+    },
+    {
+      prop: "createDate",
+      label: "创建时间",
+      width: "200",
+      tooltip: 'show-overflow-tooltip'
+    },
+    {
+      prop: "name",
+      label: "作者",
+      width: "100",
+      tooltip: 'show-overflow-tooltip'
+    },
+    {
+      prop: "status",
+      label: "状态",
+      width: "100",
+      tooltip: 'show-overflow-tooltip',
+      format: (row) => {
+        switch (row.status) {
+          case 1:
+            return '已开始'
+            break;
+          case 2:
+            return '进行中'
+            break;
+          case 3:
+            return '已结束'
+            break;
+          case 4:
+            return '已失效'
+            break;
+          default:
+            return '/'
+            break;
+        }
+      }
+    },
+    {
+      prop: "",
+      label: "操作",
+      width: "300",
+      tooltip: 'show-overflow-tooltip',
+      renderToolBox: (row: any) => {
+        console.log(row, 'row')
+        let buttons = [
+          {text: '查看', size: 'mini', callback: (row: any) => {this.see(row)}},
+          row.status == 4 && {text: '删除', type: 'danger', size: 'mini', callback: function(res:any){console.log(res,'回调函数')}},
+          (row.status == 2 || row.status == 3) && {text: '编辑', type: 'primary', size: 'mini', callback: function(res:any){console.log(res,'回调函数')}},
+          (row.status == 2 || row.status == 3 || row.status == 4) && {text: '暂停', type: 'primary', size: 'mini', callback: function(res:any){console.log(res,'回调函数')}},
+          row.status == 1 && {text: '上架', type: 'primary', size: 'mini', callback: function(res:any){console.log(res,'回调函数')}},
+          row.status == 0 && {text: '下架', type: 'primary', size: 'mini', callback: function(res:any){console.log(res,'回调函数')}},
+        ]
+        console.log(buttons, 'let buttons =')
+        return buttons
+      }
+    }
+  ]
+  see(row: any){
+    console.log(row,'see')
+  }
+  clickHandler(){
+    console.log(123465)
+  }
   blogTableData: Array<any> = [
     {
-      title: '文章标题',
+      title: '1文章标题',
       createDate: '2016-05-03',
       name: '王小虎',
       status: 1
+    },
+    {
+      title: '2文章标题',
+      createDate: '2016-05-03',
+      name: '王小虎',
+      status: 2
+    },
+    {
+      title: '3文章标题',
+      createDate: '2016-05-03',
+      name: '王小虎',
+      status: 3
+    },
+    {
+      title: '4文章标题',
+      createDate: '2016-05-03',
+      name: '王小虎',
+      status: 4
+    },
+    {
+      title: '4文章标题',
+      createDate: '2016-05-03',
+      name: '王小虎',
+      status: 0
     }
+
   ]
   onSubmit(){
     console.log(1346)
+  }
+  selectChange(list){
+    console.log(list, 'list')
   }
   handleSelectionChange(){
     console.log(456)
